7e879c7e4192f4e5990363c72691f3f66b298a57
Sarath Yalamanchili <sarath@snv-loan50-lm.corp.yahoo.com>
2014-09-08 17:48:13 -0700

    Initial Commit

---
+++ tipcalculator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  tipcalculator
+//
+//  Created by Sarath Yalamanchili on 9/8/14.
+//  Copyright (c) 2014 Sarath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
